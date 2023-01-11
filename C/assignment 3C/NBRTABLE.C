5bba8f649cb9a9088f61ef78a478f986baa2f489
sagarraj08 <sagarraj8294@gmail.com>
2023-01-11 14:53:34 +0530

    c

---
+++ C/assignment 3C/NBRTABLE.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,i;
+	clrscr();
+	printf("Enter number to get table of that number ");
+	scanf("%d",&n);
+	for(i=1;i<=10;++i)
+	{printf("%d*%d=%d\n",n,i,n*i);
+	}
+	getch();
+}
