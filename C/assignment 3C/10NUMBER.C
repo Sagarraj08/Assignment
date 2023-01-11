7ec73f2780c9721747fb9e23b034d8816c323a15
Sagarraj08 <117467589+Sagarraj08@users.noreply.github.com>
2023-01-11 13:08:26 +0530

    Add files via upload

---
+++ C/assignment 3C/10NUMBER.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a;
+	clrscr();
+	printf("Enter 10 number\n");
+	for(int i=1;i<=10;i++)
+	{
+		scanf("%d",&a);
+
+      /*	for(int j=1;j<=a;j++)
+	{
+		if(j%2==0)
+		{
+			printf("%d",a);
+		}
+	} */
+	if(a%2==0)
+	{
+	printf("%d is even",a);
+	}
+	}
+	getch();
+}
